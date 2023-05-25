979e6763608fd75d15706641f94eed6568c85d60
Santiago Suarez <sansuareze@gmail.com>
2023-05-25 16:32:00 +0200

    funcionalities done: scss pending

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,36 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: 'List was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_url, notice: 'List was successfully destroyed.'
+  end
+
+  private
+    def set_list
+      @list = List.find(params[:id])
+    end
+
+    def list_params
+      params.require(:list).permit(:name, :description)
+    end
+end
